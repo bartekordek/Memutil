bae83e18040c0896b8a74db510ff1fa808af8423
Bartłomiej Kordek <bartekordek@gmail.com>
2025-05-09 12:47:13 +0200

    Added stub for debug wrapper.

---
+++ Memutil/inc/MemUtil/IDebugWrapper.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <MemUtil/Config.hpp>
+#include <MemUtil/Generic/NonCopyable.hpp>
+#include <MemUtil/STL_Imports/STD_array.hpp>
+
+namespace MU
+{
+class IDebugWrapper
+{
+public:
+    static IDebugWrapper& getInstance();
+    virtual void init() = 0;
+    virtual void fillData( std::array<void*, G_MaxStackSize>& inOutData ) = 0;
+    virtual bool getLineByOffset( std::uint64_t offset, std::uint64_t& inOutlineNum, char* inOutName, std::size_t inOutNameSize,
+                                  std::uint64_t& outSize ) = 0;
+
+    MU_NONCOPYABLE( IDebugWrapper );
+
+protected:
+    IDebugWrapper();
+    virtual ~IDebugWrapper();
+
+private:
+
+};
+}
